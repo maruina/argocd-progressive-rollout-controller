3fa7cc2ce94d3a89e82610a276d7f606e018054c
Matteo Ruina <matteo.ruina@skyscanner.net>
2020-12-03 15:29:25 +0100

    Add sourceRef

--- api/v1alpha1/progressiverollout_types.go
+++ api/v1alpha1/progressiverollout_types.go
@@ -17,6 +17,7 @@ limitations under the License.
 package v1alpha1
 
 import (
+	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
@@ -28,8 +29,8 @@ type ProgressiveRolloutSpec struct {
 	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
 	// Important: Run "make" to regenerate code after modifying this file
 
-	// Foo is an example field of ProgressiveRollout. Edit ProgressiveRollout_types.go to remove/update
-	Foo string `json:"foo,omitempty"`
+	//SourceRef reference the ArgoCD Application Owner
+	SourceRef corev1.TypedLocalObjectReference `json:"sourceRef"`
 }
 
 // ProgressiveRolloutStatus defines the observed state of ProgressiveRollout
