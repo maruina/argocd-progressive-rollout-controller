d96094ce4e6dc8837ae95df68ea8d8c0b1a4546e
Matteo Ruina <matteo.ruina@skyscanner.net>
2020-12-13 17:49:53 +0100

    Move watchers to separate file

--- controllers/progressiverollout_controller.go
+++ controllers/progressiverollout_controller.go
@@ -21,15 +21,12 @@ import (
 	"github.com/argoproj/gitops-engine/pkg/health"
 	"github.com/go-logr/logr"
 	corev1 "k8s.io/api/core/v1"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
-	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/apimachinery/pkg/util/intstr"
 	"os/exec"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/handler"
-	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 	"sigs.k8s.io/controller-runtime/pkg/source"
 	"time"
 
@@ -44,12 +41,6 @@ type ProgressiveRolloutReconciler struct {
 	client.Client
 	Log    logr.Logger
 	Scheme *runtime.Scheme
-}
-
-// applicationWatchMapper is a support struct to filter Application events based on their owner
-type applicationWatchMapper struct {
-	client.Client
-	Log logr.Logger
 }
 
 // +kubebuilder:rbac:groups=deployment.skyscanner.net,resources=progressiverollouts,verbs=get;list;watch;create;update;patch;delete
@@ -231,45 +222,4 @@ func (r *ProgressiveRolloutReconciler) syncApp(app string) error {
 
 func (r *ProgressiveRolloutReconciler) annotateApp(app *argov1alpha1.Application) error {
 	return nil
-}
-
-// Map maps an Application event to the matching ProgressiveRollout object
-func (a *applicationWatchMapper) Map(app handler.MapObject) []reconcile.Request {
-	var requests []reconcile.Request
-	pr, err := a.ListMatchingProgressiveRollout(a.Client, app.Meta)
-	if err != nil {
-		a.Log.Error(err, "error calling ListMatchingProgressiveRollout")
-		return requests
-	}
-	if pr != nil {
-		a.Log.V(1).Info("application event matched with a progressiverollout", "app", app.Meta.GetName(), "pr", pr.Name)
-		requests = append(requests, reconcile.Request{NamespacedName: types.NamespacedName{
-			Name:      pr.Name,
-			Namespace: pr.Namespace,
-		}})
-	}
-	return requests
-}
-
-// ListMatchingProgressiveRollout filters the Application by looking at the OwnerReference
-// and returns the ProgressiveRollout referencing it
-func (a *applicationWatchMapper) ListMatchingProgressiveRollout(c client.Client, app metav1.Object) (*deploymentv1alpha1.ProgressiveRollout, error) {
-	allProgressiveRollout := &deploymentv1alpha1.ProgressiveRolloutList{}
-	err := c.List(context.Background(), allProgressiveRollout, &client.ListOptions{Namespace: app.GetNamespace()})
-
-	if err != nil {
-		return nil, err
-	}
-
-	// Check if the Application owner is reference by any ProgressiveRollout
-	for _, pr := range allProgressiveRollout.Items {
-		for _, owner := range app.GetOwnerReferences() {
-			if pr.Spec.SourceRef.Kind == owner.Kind && pr.Spec.SourceRef.Name == owner.Name && *pr.Spec.SourceRef.APIGroup == owner.APIVersion {
-				return &pr, nil
-			}
-		}
-	}
-
-	// No match
-	return nil, nil
 }
---
+++ controllers/progressiverollout_controller_watches.go
@@ -0,0 +1,61 @@
+package controllers
+
+import (
+	"context"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
+
+	"github.com/go-logr/logr"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/handler"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
+
+	deploymentv1alpha1 "github.com/maruina/argocd-progressive-rollout-controller/api/v1alpha1"
+)
+
+// applicationWatchMapper is a support struct to filter Application events based on their owner
+type applicationWatchMapper struct {
+	client.Client
+	Log logr.Logger
+}
+
+// Map maps an Application event to the matching ProgressiveRollout object
+func (a *applicationWatchMapper) Map(app handler.MapObject) []reconcile.Request {
+	var requests []reconcile.Request
+	pr, err := a.ListMatchingProgressiveRollout(a.Client, app.Meta)
+	if err != nil {
+		a.Log.Error(err, "error calling ListMatchingProgressiveRollout")
+		return requests
+	}
+	if pr != nil {
+		a.Log.V(1).Info("application event matched with a progressiverollout", "app", app.Meta.GetName(), "pr", pr.Name)
+		requests = append(requests, reconcile.Request{NamespacedName: types.NamespacedName{
+			Name:      pr.Name,
+			Namespace: pr.Namespace,
+		}})
+	}
+	return requests
+}
+
+// ListMatchingProgressiveRollout filters the Application by looking at the OwnerReference
+// and returns the ProgressiveRollout referencing it
+func (a *applicationWatchMapper) ListMatchingProgressiveRollout(c client.Client, app metav1.Object) (*deploymentv1alpha1.ProgressiveRollout, error) {
+	allProgressiveRollout := &deploymentv1alpha1.ProgressiveRolloutList{}
+	err := c.List(context.Background(), allProgressiveRollout, &client.ListOptions{Namespace: app.GetNamespace()})
+
+	if err != nil {
+		return nil, err
+	}
+
+	// Check if the Application owner is reference by any ProgressiveRollout
+	for _, pr := range allProgressiveRollout.Items {
+		for _, owner := range app.GetOwnerReferences() {
+			if pr.Spec.SourceRef.Kind == owner.Kind && pr.Spec.SourceRef.Name == owner.Name && *pr.Spec.SourceRef.APIGroup == owner.APIVersion {
+				return &pr, nil
+			}
+		}
+	}
+
+	// No match
+	return nil, nil
+}
